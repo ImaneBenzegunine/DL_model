15bda3afe559e82eb690696f8de93281bf3ebf28
ImaneBenzegunine <imane.benzegunine@gmail.com>
2025-04-17 19:40:01 +0100

    first commit

---
+++ Scripts/paths.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+
+
+PATH_PROCESSED_CSV = os.path.join(BASE_DIR, "data", "outputTransfomed", "processed.csv")
